8f1cdbe9901f93f2034f3fedaa9d9980b5fb33f2
jai <jai@pearlorganisation.com>
2024-10-12 16:58:14 +0530

    updatse

--- src/routes/product.js
+++ src/routes/product.js
@@ -4,7 +4,7 @@ import { verifyTokenMiddleware } from "../middlewares/verifyTokenMiddleware.js";
 import { addProduct, getProducts } from "../controller/product.js";
 
 const productRouter = express.Router();
-productRouter.route("/").post(verifyTokenMiddleware, addProduct);
-productRouter.route("/:id").get(getProducts)
+productRouter.route("/").get(verifyTokenMiddleware, getProducts).post(verifyTokenMiddleware, addProduct);
+
 
 export default productRouter;
