f9499cd534c9f914dd6f0af3b5951d2462829412
jai <jai@pearlorganisation.com>
2024-10-28 12:25:29 +0530

    updates

--- src/controller/customSettings.js
+++ src/controller/customSettings.js
@@ -4,7 +4,7 @@ import { asyncHandler } from "../utils/errorHandler/asyncHandler.js";
 
 export const getCustomSettings = asyncHandler(async (req, res) => {
   const result = await customSettingsModel.findOne({
-    adminId: new mongoose.Types.ObjectId(`${req?.adminId}`),
+    adminId: req?.adminId,
   });
   res.status(200).json({ status: true, data: result });
 });
